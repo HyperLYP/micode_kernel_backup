aff16f0af4d496bfa498550725895204069102fc
101012270 <gaominlong@huaqin.com>
2020-07-28 16:01:31 +0800

    [J15SR-61731]{type:BSP;pro:J15S}关闭自动亮度，亮度调至最低时，偶线亮度闪烁[1/1]
    [Problem]
    由于CABC的功能的开启，在最低亮度下，会进行亮度的调节，而最低亮度下，每阶亮度变化明显,
    故在最低亮度下操作时，偶现亮度闪烁
    
    [Solution]
    在最低亮度下，背光阶级变化时，让亮度变化柔和一点。
    
    [Test]
    Pass
    
    [Comment]
    N/A
    
    Change-Id: I6fb32743a0fcef983c1b5c493190bbe407ed0eca

--- drivers/misc/mediatek/leds/mt6768/ti-lmu-backlight-core.c
+++ drivers/misc/mediatek/leds/mt6768/ti-lmu-backlight-core.c
@@ -381,6 +381,13 @@ int lm3697_set_brightness(int brightness)
 	//printk(KERN_INFO "[bkl][before]%s brightness = %d\n", __func__, brightness);
 	//return ti_lmu_backlight_update_brightness_register(bl_chip->lmu_bl, brightness);
 
+#ifdef CONFIG_TARGET_PRODUCT_MERLINCOMMON
+	if ((brightness < 6) && (brightness > 2)) {//HQ-61731
+		brightness = 6;
+		printk("%s: lyd_lowest_brightness, brightness = %d", __func__, brightness);
+	}
+#endif
+
 	return ti_lmu_backlight_set_brightness(brightness);
 }
 
