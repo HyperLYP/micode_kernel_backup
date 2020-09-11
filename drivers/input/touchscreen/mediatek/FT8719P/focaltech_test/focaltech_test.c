7f555b648c1800cc304300fc50138e582a05f52c
101011743 <zhangtisheng@huaqin.com>
2020-09-12 00:27:38 +0800

    [J15S]解决基线升级冲突2
    Change-Id: Idade9fc80b680e39a29825debaf144e50e315a61

--- drivers/input/touchscreen/mediatek/FT8719P/focaltech_test/focaltech_test.c
+++ drivers/input/touchscreen/mediatek/FT8719P/focaltech_test/focaltech_test.c
@@ -1705,8 +1705,9 @@ static int get_channel_num(struct fts_test *tdata)
 	tdata->node.rx_num = rx_num;
 	if (IC_HW_INCELL == tdata->func->hwtype)
 		tdata->node.channel_num = tx_num * rx_num;
-	else if (IC_HW_MC_SC == tdata->func->hwtype)
+	else if (IC_HW_MC_SC == tdata->func->hwtype){
 		tdata->node.channel_num = tx_num + rx_num;
+    }
 		tdata->node.node_num = tx_num * rx_num;
     }
 
