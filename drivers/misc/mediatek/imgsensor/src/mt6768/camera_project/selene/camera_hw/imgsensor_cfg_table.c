297d75ebf90a1192458ab1e89f00818f03feccef
p-yangjiayuan <yang_jiayuan@hoperun.com>
2021-04-29 16:36:21 +0800

    Camera:K19A:BSP:Power down sequence of main camera
    HQ-124121
    
    1.Problem：power down sequence does not match datasheet requirement
    2.Solution: Delay RST power down 5ms
    3.Test：Local test passed
            RST power down later than MCLK
    
    Change-Id: Idf854976bc5523a85433e5d936952708bf39807c
    Signed-off-by: yang_jiayuan@hoperun.com

--- drivers/misc/mediatek/imgsensor/src/mt6768/camera_project/selene/camera_hw/imgsensor_cfg_table.c
+++ drivers/misc/mediatek/imgsensor/src/mt6768/camera_project/selene/camera_hw/imgsensor_cfg_table.c
@@ -168,7 +168,7 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 		SENSOR_DRVNAME_OV50C40_OFILM_MAIN_MIPI_RAW,
 		{
             {SensorMCLK, Vol_High, 0},
-			{RST, Vol_Low, 1},
+			{RST, Vol_Low, 5},
 			{AVDD, Vol_2800, 1},
 			{DOVDD, Vol_1800, 1},
 			{DVDD, Vol_1200, 1},
@@ -181,7 +181,7 @@ struct IMGSENSOR_HW_POWER_SEQ sensor_power_sequence[] = {
 		SENSOR_DRVNAME_OV50C40_QTECH_MAIN_MIPI_RAW,
 		{
             {SensorMCLK, Vol_High, 0},
-			{RST, Vol_Low, 1},
+			{RST, Vol_Low, 5},
 			{AVDD, Vol_2800, 1},
 			{DOVDD, Vol_1800, 1},
 			{DVDD, Vol_1200, 1},
