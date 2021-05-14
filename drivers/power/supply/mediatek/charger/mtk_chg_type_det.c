97acb9ea83b22c313eabf0ea08b3bfe1b4f2bdfa
102012809 <wangqi11@huaqin.com>
2021-05-14 14:50:26 +0800

    [HQ-129052]{type:charger; pro:K19A}K19A 通话限流影响[1/1]
    [Problem]
    电流设置问题
    
    [Solution]
    修改电流设置
    
    [Test]
    验证pass
    
    [Comment]
    N/A
    
    Change-Id: Ie21070d11c96cd04df995569e440d798d2d3e9b0
    (cherry picked from commit 15f0dd9e64c037791d6be20179743bbbd11d4bfb)

--- drivers/power/supply/mediatek/charger/mtk_chg_type_det.c
+++ drivers/power/supply/mediatek/charger/mtk_chg_type_det.c
@@ -249,10 +249,9 @@ static int mt_charger_get_property(struct power_supply *psy,
 		val->intval = mtk_chg->chg_type;
 		break;
 	case POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX:
-		/*K19A HQ-129052 K19A charger of thermal current limit by wangqi at 2021/5/13 start*/
-		charger_dev_get_charging_current(mtk_chg->chg1_dev, &call_mode);
+		/*K19A HQ-129052 K19A charger of thermal current limit by wangqi at 2021/5/14 start*/
 		val->intval = call_mode;
-		/*K19A HQ-129052 K19A charger of thermal current limit by wangqi at 2021/5/13 end*/
+		/*K19A HQ-129052 K19A charger of thermal current limit by wangqi at 2021/5/14 end*/
 		break;
 	default:
 		return -EINVAL;
